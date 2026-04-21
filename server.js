799d623dda5b038e3138193b135f0afb8c00ace6
Venom2-0z <132488342+Venom2-0z@users.noreply.github.com>
2026-04-21 09:49:42 +0000

    npm

---
+++ server.js
@@ -0,0 +1,10 @@
+const expess = require('express');
+const app = expess();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+}); 
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
