ac8bb0931be2f34259bf83a55a40ff869afb2b87
Venom2-0z <132488342+Venom2-0z@users.noreply.github.com>
2026-04-23 06:02:56 +0000

    ensuring proper linting

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const expess = require('express');
-const app = expess();
-const PORT = 3000;
-let x;
-app.get('/', (req, res) => {
-    res.send('Hello World');
-}); 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+const express = require('express');
+
+const app = express();
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
